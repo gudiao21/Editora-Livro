9dcb33eca1f0e68dd12d3c02016498f8dd1bf838
gudiao21 <lucianodantas53@gmail.com>
2023-10-12 05:55:36 -0300

    Feat(migration): Change assemblies migration.

--- db/migrate/20230924233523_create_assemblies.rb
+++ db/migrate/20230924233523_create_assemblies.rb
@@ -2,7 +2,7 @@ class CreateAssemblies < ActiveRecord::Migration[7.0]
   def change
     create_table :assemblies do |t|
       t.string :name
-
+      t.references :book, null: false, foreign_key: true
       t.timestamps
     end
   end
