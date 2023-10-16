eae890c82d7447eed86e0d7962c1f1ba2cbe4033
gudiao21 <lucianodantas53@gmail.com>
2023-10-16 04:12:07 -0300

    Feat(routes): Create it.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
   get 'welcome/index'
-  devise_for :users, skip: :sessions
+  devise_for :users
 
    devise_scope :user do
      root to: 'welcome#index'
    end
-
-  #root to:'devise/sessions#new'
 
   resources :parts
   resources :assemblies
