ba1c4c7056977cc68f95ef2633a29e4ed31a4255
gudiao21 <lucianodantas53@gmail.com>
2023-10-18 09:02:29 -0300

    Feat(manifest): Change it.

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,7 +1,8 @@
+//= link_tree ../images
 //= link tailwind.css
 //= link application.css
 //= link application.js
 //= link controllers/application.js
 //= link controllers/hello_controller.js
 //= link controllers/index.js
-//= link logo.png
+//= link_tree ../builds
