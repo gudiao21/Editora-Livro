68cf6640e2098bffbf1b1cc55653df36ef928bca
gudiao21 <lucianodantas53@gmail.com>
2023-10-20 18:15:10 -0300

    Feat(migration): Create 'db/migrate/20230923225418_create_books.rb' file.

--- db/migrate/20230923225418_create_books.rb
+++ db/migrate/20230923225418_create_books.rb
@@ -1,7 +1,6 @@
 class CreateBooks < ActiveRecord::Migration[7.0]
   def change
     create_table :books do |t|
-      t.string :name
       t.datetime :published_at
       t.references :author, null: false, foreign_key: true
 
