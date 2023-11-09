9f437780498cf2cf712c1edf1597c51adbdb62ca
gudiao21 <lucianodantas53@gmail.com>
2023-11-09 08:39:05 -0300

    Feat: Change 'config/application.rb' file.

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module EditoraLivro
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    # Defina o idioma padrão para português do Brasil (pt-BR)
+    config.i18n.default_locale = 'pt-BR'
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
