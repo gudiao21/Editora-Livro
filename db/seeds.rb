af30f91e7d7ba37cb98abac022ecb2b78a585261
gudiao21 <lucianodantas53@gmail.com>
2023-11-18 08:40:08 -0300

    Feat(seed): Change 'db/seeds.com' file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,51 +3,38 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-require 'factory_bot_rails'
-require 'faker'
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
 
 # Crie um usuário de exemplo:
-user = User.create!(email: 'marcodotcastro@gmail.com', password: 'abc12345abc', password_confirmation: 'abc12345abc')
-
-# Crie 2 fornecedores
-suppliers = FactoryBot.create_list(:supplier, 2)
-
-# Crie contas associadas a cada fornecedor
-suppliers.each do |supplier|
-  FactoryBot.create(:account, supplier: supplier)
+begin
+  user = User.create!(email: 'marcodotcastro@gmail.com', password: 'abc12345abc', password_confirmation: 'abc12345abc')
+  puts "Usuário criado com sucesso!"
+rescue ActiveRecord::RecordInvalid => e
+  puts "Erro ao criar o usuário: #{e.message}"
+  puts e.record.errors.full_messages
 end
 
-# Crie 5 autores
-#author = FactoryBot.create(:author)
-5.times do
-  FactoryBot.create(:author, name: Faker::Book.author, cpf: Faker::Number.number(digits: 11))
-end
+# authors
+Author.create(name: "John Doe", cpf: "12345678901")
+Author.create(name: "Jane Smith", cpf: "23456789012")
+Author.create(name: "Alan Johnson", cpf: "34567890123")
 
-# Crie 5 books com nomes aleatórios e ISBNs aleatórios
-5.times do
-  Book.create(title: Faker::Book.title, author: Author.first, isbn: Faker::Code.isbn)
-end
+# books
+Book.create(title: "Clean Code: A Handbook of Agile Software Craftsmanship", published_at: "2008-08-11", author_id: 1, isbn: "9780132350884")
+Book.create(title: "The Pragmatic Programmer: Your Journey to Mastery", published_at: "2019-11-20", author_id: 2, isbn: "9780135957059")
+Book.create(title: "Design Patterns: Elements of Reusable Object-Oriented Software", published_at: "1994-10-31", author_id: 3, isbn: "9780201633610")
+Book.create(title: "Refactoring: Improving the Design of Existing Code", published_at: "1999-07-08", author_id: 1, isbn: "9780201485677")
+Book.create(title: "Code Complete: A Practical Handbook of Software Construction", published_at: "2004-06-09", author_id: 2, isbn: "9780735619678")
 
-# seeds.rb
+# suppliers
+Supplier.create(name: "Papel e Cia Produtos de Papelarias LTDA", cnpj: "19518277000139")
+Supplier.create(name: "Livraria Ler Paqpelaria LTDA", cnpj: "08596371000190")
 
-# Crie 5 registros para Parts
-5.times do
-  Part.create!(
-    name: Faker::Lorem.word,  # Use a gem como Faker para gerar nomes fictícios
-  # Outros atributos do modelo Part
-    )
-end
+# accounts
+Account.create(account_number: "261533", supplier_id: 1, digit: "9")
+Account.create(account_number: "111111", supplier_id: 2, digit: "6")
+#Account.create(account_number: "111112", supplier_id: 3, digit: "8")
+#Account.create(account_number: "122223", supplier_id: 4, digit: "3")
+#Account.create(account_number: "123223", supplier_id: 5, digit: "8")
 
-# Crie 5 registros para Assembly e associe cada um deles a partes aleatórias
-5.times do
-  assembly = Assembly.create!(
-    name: Faker::Lorem.sentence,  # Use a gem como Faker para gerar nomes fictícios
-    book_id: Book.pluck(:id).sample  # Suponha que você tenha registros na tabela de Books
-  # Outros atributos do modelo Assembly
-  )
-
-  # Associe partes aleatórias ao assembly
-  assembly.parts << Part.all.sample(rand(1..5))
-end
