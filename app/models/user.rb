a09ef176d3b4d7a4a49a0421d496d2c3052abdb4
gudiao21 <lucianodantas53@gmail.com>
2023-11-22 09:52:44 -0300

    Feat(model): Change 'app/models/user.rb' file.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :omniauthable
+  validates :email, presence: true, uniqueness: true
 end
