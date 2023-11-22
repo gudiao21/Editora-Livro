1a21387c00c85b06f0da4b221081b6b0164da60e
gudiao21 <lucianodantas53@gmail.com>
2023-11-22 09:20:02 -0300

    Feat(checkaccountdigit): Change 'app/business/check_account_digit.rb' file.

--- app/business/check_account_digit.rb
+++ app/business/check_account_digit.rb
@@ -1,24 +1,19 @@
 class CheckAccountDigit
-  def initialize(account_number, digit)
+  def initialize(account_number)
     @account_number = account_number
-    @digit = digit
   end
 
-  def check_digit?
+  def check_digit
     i = @account_number.length + 1
     j = 0
     sum_product = 0
+
     while i > 1
       sum_product += @account_number[j].to_i * i
       i -= 1
       j += 1
     end
 
-    calculated_digit = (sum_product * 10) % 11
-
-    puts "Provided Digit: #{@digit}"
-    puts "Calculated Digit: #{calculated_digit}"
-
-    @digit.to_s == calculated_digit.to_s
+    (sum_product * 10) % 11
   end
 end
