a17884a2e568f3b0539368927a3afefb0c5d87ea
gudiao21 <lucianodantas53@gmail.com>
2023-11-22 17:10:43 -0300

    Feat(model): Change 'app/models/account.rb' file.

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,13 +1,15 @@
 class Account < ApplicationRecord
   belongs_to :supplier
   validates :account_number, presence: true, uniqueness: true
-  validate :account_digit_is_valid?
+
+  before_validation :calculate_and_set_digit, on: :create
 
   scope :search, ->(query) { where("account_number like ?", "%#{query}%") }
+
   private
 
-  def account_digit_is_valid?
+  def calculate_and_set_digit
     d = CheckAccountDigit.new(account_number)
-    errors.add(:digit, "Dígito não válido! Por gente lesa, verifique o número da conta!") unless d.check_digit?
+    self.digit = d.check_digit
   end
 end
