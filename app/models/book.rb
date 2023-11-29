88865120acbd107c78094edd1d2491b021ac4ac0
gudiao21 <lucianodantas53@gmail.com>
2023-11-29 09:33:55 -0300

    Feat(Model): Change 'app/models/book.rb' file.

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,15 @@
 class Book < ApplicationRecord
   belongs_to :author
   has_many :assemblies
+  validates :title, presence: true, length: {minimum: 3, maximum: 100}
+  validate :isbn_is_valid?
+
+  scope :search, ->(query) { where("title like ?", "%#{query}%") }
+  scope :by_author_name, ->(query) { joins(:author).where("LOWER(authors.description) LIKE ?", "%#{query.downcase}%") }
+
+  private
+
+  def isbn_is_valid?
+    errors.add(:isbn, "ISBN inválido! Por favor, reveja o número.") unless ISBN.valid?(isbn)
+  end
 end
