d5b43dc7b0abd9e9a78b4512fbc90e829da6c234
gudiao21 <lucianodantas53@gmail.com>
2023-11-29 09:05:50 -0300

    Feat(Model): Change 'app/models/author.rb' file.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,12 @@
 class Author < ApplicationRecord
   has_many :books, dependent: :destroy
+  validates :name, presence: true
+  validates :name, length: { minimum: 3, maximum: 60 }
+  validate :cpf_is_valid?
+
+  private
+
+  def cpf_is_valid?
+    errors.add(:cpf, "Formato de CPF errado! Por favor, cheque os números.") unless CPF.valid?(cpf)
+  end
 end
