736db813c869921c58fb6b0fc2f62604d8a4f589
gudiao21 <lucianodantas53@gmail.com>
2023-12-03 16:43:59 -0300

    Feat(model): Change 'app/models/assembly.rb' file.

--- app/models/assembly.rb
+++ app/models/assembly.rb
@@ -2,5 +2,7 @@ class Assembly < ApplicationRecord
   belongs_to :book
   has_and_belongs_to_many :parts, join_table: "assembly_parts"
 
-  scope :search_by_name, ->(query) { where("name ILIKE ?", "%#{query}%") }
+  scope :search_by_name, ->(query) {
+    joins(:parts).where("parts.name ILIKE ?", "%#{query}%")
+  }
 end
