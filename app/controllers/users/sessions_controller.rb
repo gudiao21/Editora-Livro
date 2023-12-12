0549e0c4be99798b99082b0ec4f531598155965b
gudiao21 <lucianodantas53@gmail.com>
2023-12-12 14:41:00 -0300

    Feat(controller): Change 'app/controllers/users/sessions_controller.rb' file.

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,6 +2,7 @@ class Users::SessionsController < Devise::SessionsController
   skip_before_action :require_no_authentication
 
   def after_sign_in_path_for(resource)
-    render layout: 'app/views/layouts/application.html.erb'
+    #render layout: 'app/views/layouts/application.html.erb'
+    super
   end
 end
